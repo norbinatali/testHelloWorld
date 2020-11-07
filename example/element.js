959765db614ca126ed876c4b0cbb22d0f59170a1
norbinatali <norbi>
2020-11-07 21:27:04 +0200

    First commit

---
+++ example/element.js
@@ -0,0 +1,8 @@
+import '../src/x-550094-hello-world';
+
+const el = document.createElement('DIV');
+document.body.appendChild(el);
+
+el.innerHTML = `		
+<x-550094-hello-world></x-550094-hello-world>
+`;
---
+++ src/x-550094-hello-world/__tests__/test.x-550094-hello-world.js
@@ -0,0 +1,5 @@
+describe('x-550094-hello-world Test', () => {
+	it('should be true', () => {
+		expect(true).toBe(true);
+	});
+});
